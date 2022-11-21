0591a8c9b59d703a094685d52ef9567fa31a4b4a
Nomta <nomta@outlook.com>
2022-11-21 17:41:51 +0300

    hotels app component

--- src/components/HotelsApp.vue
+++ src/components/HotelsApp.vue
@@ -1,19 +1,20 @@
 <template>
   <v-container>
     <v-row>
-      <v-col cols="4">
-        <hotels-filter />
+      <v-col cols="3">
+        <hotels-filter :hotels="hotels" @filter="filterHotels" />
       </v-col>
-      <v-col cols="8">
-        <hotels-list />
+      <v-col cols="9">
+        <hotels-list :hotels="filteredHotels" />
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-import HotelsFilter from '@/components/HotelsFilter.vue';
+import HotelsFilter from '@/components/HotelsFilters.vue';
 import HotelsList from '@/components/HotelsList.vue';
+import { hotels } from '@/fixtures/hotels';
 
 export default {
   name: 'HotelsApp',
@@ -21,7 +22,20 @@ export default {
   components: {
     HotelsFilter,
     HotelsList,
-  }
+  },
+
+  data() {
+    return {
+      hotels,
+      filteredHotels: [...hotels]
+    }
+  },
+
+  methods: {
+    filterHotels(value) {
+      this.filteredHotels = value;
+    }
+  },
 }
 </script>
 
