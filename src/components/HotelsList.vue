469a1330f14640b808ed36ff4810b2dd2f7c9bc4
Nomta <nomta@outlook.com>
2022-11-21 17:32:47 +0300

    hotels list component

--- src/components/HotelsList.vue
+++ src/components/HotelsList.vue
@@ -1,18 +1,25 @@
 <template>
   <div>
-    <hotels-list-item />
+    <hotels-list-item v-for="hotel in hotels" :hotel="hotel" :key="hotel.id" />
   </div>
 </template>
 
 <script>
 import HotelsListItem from '@/components/HotelsListItem.vue';
+import { hotels } from '@/fixtures/hotels';
 
 export default {
   name: 'HotelsList',
 
   components: {
     HotelsListItem,
-  }
+  },
+
+  data() {
+    return {
+      hotels
+    }
+  },
 }
 </script>
 
